5b026ff6d2dfaf7142c93b9db53b25573daa635d
hvirix web2025-4 <sofiaasuk3@gmail.com>
2025-04-14 19:51:27 +0300

    second part

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { program } = require ('commander');
 const fs = require ('node:fs');
 const http = require ('http');
+const xml = require("fast-xml-parser");
 
 program
     .option('-h, --host <address>')
@@ -29,12 +30,28 @@ if (!fs.existsSync(options.input)){
     return;
 }
 
-
-
 const requestListener = function (req, res) {
+    fs.readFile(options.input, 'utf8', (err, data) => {
+        let parsedData = JSON.parse(data);
+        const builder = new xml.XMLBuilder({format : true});
+        const xmlStr = builder.build(parsedData);
+        const parser = new xml.XMLParser();
+        const obj = parser.parse(xmlStr);
+        const array = [];
+        for (const item in obj) {
+            array.push(obj[item].rate);
+        }
+        let max_rate = Math.max(...array);
+        const objToSave = {data: {max_rate: max_rate}};
+        const newXML = builder.build(objToSave);
+        fs.writeFile("test.xml", newXML);
 
+    });
 }
+
+
 const server = http.createServer(requestListener);
 server.listen(options.port, options.host, () => {
     console.log(`Server is running on ${options.host}:${options.port}`);
 });
+
